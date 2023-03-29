28ce3368e5b57338a3bf98e65734a4e3e73e585c
Sergey Brazhnik <sergey.brazhnik@cleverbase.com>
2023-03-29 14:39:58 +0300

    init

---
+++ NoizeLib/NoizeLib.h
@@ -0,0 +1,18 @@
+//
+//  NoizeLib.h
+//  NoizeLib
+//
+//  Created by Sergey Brazhnik on 29.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NoizeLib.
+FOUNDATION_EXPORT double NoizeLibVersionNumber;
+
+//! Project version string for NoizeLib.
+FOUNDATION_EXPORT const unsigned char NoizeLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoizeLib/PublicHeader.h>
+
+
